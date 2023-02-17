8298710a5f870b395c6b5425c3c921c70be16751
Liza <kuznecovaliza11@yandex.ru>
2023-02-18 02:00:48 +0700

    палиндром

---
+++ Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿int GetReverse(int Value)
+{
+    int ValueTMP = Value;
+    int Reverse = 0;
+    while (ValueTMP > 0)
+    {
+        Reverse = Reverse * 10 + ValueTMP % 10;
+        ValueTMP /= 10;
+    }
+    return Reverse;
+}
+
+Console.WriteLine("Введите палиндром");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Reverse = GetReverse(Value);
+ if (Value == Reverse)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
