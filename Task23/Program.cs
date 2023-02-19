cafdc3ae32acc22b8898f75173902438fddb9178
Liza <kuznecovaliza11@yandex.ru>
2023-02-20 03:43:16 +0700

    Куб

---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите длину массиву");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] Cube = new int[N];
+for (int index = 0; index < N; index++)
+{
+    Cube[index] = Convert.ToInt32(Math.Pow((index + 1),3));
+    Console.WriteLine(Cube[index]);
+}
